3c85dcd49dc2be268478611b4eff34119668f982
Felippe <113641116+Felippe-nobre@users.noreply.github.com>
2025-01-01 16:17:20 -0300

    .

---
+++ day1.js
@@ -0,0 +1,27 @@
+//!Aula 1- variaveis
+// ? oque sao variaveis
+let nome = "Felippe";
+let cidade = "São Paulo"
+
+console.log("Olá, meu nome é ",nome, "e eu moro em", cidade);
+
+//? tipos de variaveis
+//! string
+let estado = "SP";
+
+//! number
+let idade = 21;
+
+//! boolean
+let maiorDeIdade = true;
+
+//!array
+let lista = [true, false, true];
+
+//!object
+let pessoa = {
+    nome: "Felippe",
+    cidade: "São Paulo",
+    idade: 21
+}
+console.log(pessoa.idade);
